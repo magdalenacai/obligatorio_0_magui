3c070ae38f6eeadc754e160e0cb1d2277404f247
magdalenacai <magdalenacairo2000@gmail.com>
2020-10-25 11:54:55 -0300

    Entrega 6

--- js/cart.js
+++ js/cart.js
@@ -88,6 +88,84 @@ document.getElementById("total").addEventListener("change", function () {
   actualizar();
 })
 
+function tarjetaCredito() {
+    const HTMLContainer = document.querySelector("#paymentOptionContent")
+    HTMLContainer.innerHTML = `
+    <div class="form-group">
+      <label for="numeroTarjeta">Número de tarjeta (13 Digitos)</label>
+      <input type="tel" id="numeroTarjeta" class="form-control" pattern="[0-9]{13}" required>
+
+      <label for="CVV">CVV (Codigo de Seguridad de 3 Digitos)</label>
+        <input type="tel" id="CVV" class="form-control" pattern="[0-9]{3}" required>
+
+    </div>
+    <div class="form-group">
+      <label for="cuotas">Cantidad de cuotas</label>
+      <select class="form-control" id="cuotas" required>
+        <option>1</option>
+        <option>2</option>
+        <option>3</option>
+        <option>6</option>
+        <option>8</option>
+        <option>10</option>
+        <option>12</option>
+      </select>
+    </div>
+    `
+  }
+  
+  function banco() {
+    const HTMLContainer = document.querySelector("#paymentOptionContent")
+    HTMLContainer.innerHTML = `
+    <div class="form-group">
+      <label for="transferencia">Banco</label>
+      <select class="form-control" id="transferencia" required>
+        <option>BROU</option>
+        <option>Itau</option>
+        <option>Scotiabank</option>
+        <option>BBVA</option>
+        <option>Santander</option>
+      </select>
+    </div>
+    <div class="form-group">
+      <label for="numCuenta">Número de cuenta</label>
+      <input type="tel" id="numCuenta" class="form-control" required>
+    </div>
+    `
+  }
+  
+  function validar() {
+    paymentForm = document.querySelector("#paymentForm")
+    paymentForm.addEventListener('submit', (e) => {
+      e.preventDefault()
+      e.stopPropagation()
+      if (paymentForm.checkValidity()) {
+        document.querySelector('#successAlertContainer').classList.remove('d-none')
+        $('#paymentModal').modal('hide')
+      }
+    })
+  }
+  
+  document.addEventListener("DOMContentLoaded", function (e) {
+    getJSONData(CART_INFO_URL).then(function (resultObj) {
+      if (resultObj.status === "ok") {
+        articles = resultObj.data.articles;
+        mostrarArticulos(articles);
+      }
+    });
+  
+    shippingForm = document.querySelector("#shippingForm")
+    shippingForm.addEventListener('submit', (e) => {
+      e.preventDefault()
+      e.stopPropagation()
+      if (shippingForm.checkValidity()) {
+        $('#paymentModal').modal('show')
+        validar()
+      }
+      shippingForm.classList.add('was-validated')
+    })
+  });
+
 
 
 
